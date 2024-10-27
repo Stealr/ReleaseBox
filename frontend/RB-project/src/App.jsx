8fbdc83db0bc1a3fa5e7fb9f2799ee9fc861ba77
Stealr <baranovm003@yandex.ru>
2024-10-27 20:51:06 +0300

    add started frontend

---
+++ frontend/RB-project/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h2> Just nothing here yet :) </h2>
+      Project RB
+    </div>
+  );
+}
+
+
+export default App;
