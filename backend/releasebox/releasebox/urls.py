997fc351b4d0497ed074c868bc16ac535cc4c74a
Excellent150 <danilakdanila@yandex.ru>
2024-11-03 11:24:13 +0300

    1 рабочая версия
    1 модель, с представлением. скорее всего готова к подключению к React'у

---
+++ backend/releasebox/releasebox/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import re_path as url
+from backend_api.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', GameInfoView.as_view())
+]
