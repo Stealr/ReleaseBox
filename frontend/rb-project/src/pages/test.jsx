516add53df60d3fd7e68feb61149abdd6da459a1
Stealr <baranovm003@yandex.ru>
2024-11-04 13:49:57 +0300

    delete node_modules from git and add view of all games from data

--- frontend/rb-project/src/pages/test.jsx
+++ frontend/rb-project/src/pages/test.jsx
@@ -12,7 +12,7 @@ function Test() {
 
 
     useEffect(() => {
-        axios.get('http://localhost:8000/api/.../')
+        axios.get('http://localhost:8000')
             .then(response => {
                 setData(response.data);
             })
@@ -25,10 +25,19 @@ function Test() {
         <div>
             <h1>Data from Django</h1>
             <ul>
-                {data.map(item => (
-                    <li key={item.id}>{item}</li> // замените на соответствующие поля
+                {data.map(data => (
+                    <li key={data.id}>
+                        <h2>{data.name}</h2>
+                        <p><strong>Released:</strong> {data.released}</p>
+                        <p><strong>Platform:</strong> {data.platform}</p>
+                        <p><strong>Dev:</strong></p>
+                        <p><strong>Push:</strong></p>
+                        <p><strong>Genres:</strong> {data.genres}</p>
+                        <br/>
+                    </li>
                 ))}
             </ul>
+            {console.log(data)}
         </div>
     )
 }
