ee8ec4ed9e919fc6b597277c62ea7bab2287032c
Stealr <baranovm003@yandex.ru>
2024-11-13 20:33:54 +0300

    add footer

---
+++ frontend/rb-project/src/Components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import "./footer_style.css";
+
+function Home() {
+    return (
+        <div className="footer">
+            <div className='content-footer'>
+                <img src='/src/assets/LOGO.png' alt='logo' />
+
+                <nav className="navigation">
+                    <a href="home">Home</a>
+                    <a href="games">Games</a>
+                    <a href="calendar">Calendar</a>
+                    <a href="reviews">Reviews</a>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Home
--- frontend/rb-project/src/pages/home.jsx
+++ frontend/rb-project/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Header from "/src/Components/Header/Header.jsx";
+import Footer from "/src/Components/Footer/Footer.jsx"
 
 function Home() {
 
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     );
 }
