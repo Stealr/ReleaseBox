60b59dd3a61fd601dacdb13bc6c431902a035238
Stealr <baranovm003@yandex.ru>
2024-11-23 23:57:13 +0300

    change login file without css

--- frontend/rb-project/src/pages/Auth/Login/login.jsx
+++ frontend/rb-project/src/pages/Auth/Login/login.jsx
@@ -4,11 +4,18 @@ import "./login.css";
 function Login() {
 
     return (
-        <div>
-            <div className="main-content">
-                <div className="container">
-
+        <div className='main-content'>
+            <div className='auth-cont'>
+                Log in
+                <div className='login-form'>
+                    <input/>
+                    <input/>
+                    {/* Может заменить потом checkbox */}
+                    <input type="checkbox" />  remember me
+                    <button>Log in</button>
                 </div>
+                New User? Sign up here
+                Forgot your password?
             </div>
         </div>
     );
