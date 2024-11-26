cef13bb46d93399512b4c822f55b686d1ca22e04
Stealr <baranovm003@yandex.ru>
2024-11-26 10:53:41 +0300

    Стилизация страницы log in

--- frontend/rb-project/src/pages/Auth/Login/login.jsx
+++ frontend/rb-project/src/pages/Auth/Login/login.jsx
@@ -6,16 +6,24 @@ function Login() {
     return (
         <div className='main-content'>
             <div className='auth-cont'>
-                Log in
-                <div className='login-form'>
-                    <input/>
-                    <input/>
-                    {/* Может заменить потом checkbox */}
-                    <input type="checkbox" />  remember me
-                    <button>Log in</button>
+                <form className="login-form">
+                    <h1>Log in</h1>
+                    <input className='input-auth' type="email" placeholder="Email" required />
+                    <input className='input-auth' type="password" placeholder="Password" required />
+                    <div className="remember-me">
+                        <input type="checkbox" id="remember" />
+                        <label htmlFor="remember">Remember me</label>
+                    </div>
+                    <button type="submit">Log in</button>
+                </form>
+                <div className="login-links">
+                    <p>
+                         <a href="#">Sign up here</a>
+                    </p>
+                    <p>
+                        <a href="#">Forgot password</a>
+                    </p>
                 </div>
-                New User? Sign up here
-                Forgot your password?
             </div>
         </div>
     );
