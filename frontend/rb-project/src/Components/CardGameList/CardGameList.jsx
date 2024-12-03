8c3f18a96e4f1490907cc1b3f8ccc8aa5363b9a9
Stealr <baranovm003@yandex.ru>
2024-12-03 14:15:08 +0300

    добавлена менюшка добавления игры в колекцию

--- frontend/rb-project/src/Components/CardGameList/CardGameList.jsx
+++ frontend/rb-project/src/Components/CardGameList/CardGameList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./CardGameList.css";
 
 //TODO: Добавить вслывающиеся окно при новедении на иконку с платформой. Название платформы
@@ -8,6 +8,19 @@ function CardGameList({ gameId, name, released, platform, genres, metacritic, im
     const platform_icon_define = (name) => {
         return `/src/assets/platforms/${name.toLowerCase()}.svg`;
     }
+
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [rating, setRating] = useState(0); // Состояние для хранения рейтинга
+
+    // Обработчик для клика по кнопке "Add"
+    const handleAddClick = () => {
+        setIsMenuVisible(!isMenuVisible); // Переключаем видимость меню
+    };
+
+    const handleRatingClick = (value) => {
+        setRating(value); // Устанавливаем рейтинг
+    };
+
     return (
         <div className='card-game-list'>
             <div className='card-top'>
@@ -22,11 +35,37 @@ function CardGameList({ gameId, name, released, platform, genres, metacritic, im
                         <img
                             src='/src/assets/plus.svg'
                             alt="Add button"
-                            onClick={() => addCollection(gameId)}
+                            onClick={handleAddClick}
                         />
+                        {isMenuVisible && (
+                            <div className='menu'>
+                                <button className='menu-item'>Done</button>
+                                <button className='menu-item'>Playing</button>
+                                <button className='menu-item'>Favourite</button>
+                                <button className='menu-item'>Wishlist</button>
+                                <button className='menu-item'>Abandoned</button>
+                                <button className='menu-item del'>Delete</button>
+                                {/* Блок рейтинга */}
+                                <div className='rating'>
+                                    <div className='rating-title'>Выставите рейтинг:</div>
+                                    <div className='rating-buttons'>
+                                        {[0, 1, 2, 3, 4, 5].map((value) => (
+                                            <button
+                                                key={value}
+                                                className={`rating-button ${rating === value ? 'selected' : ''}`}
+                                                onClick={() => handleRatingClick(value)}
+                                            >
+                                                {value}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
+
             <div className='card-bottom'>
                 <div className='info'>
                     <div className='game-name' onClick={() => handleGameClick(name, gameId)}>
